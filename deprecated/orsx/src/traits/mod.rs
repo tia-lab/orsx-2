fe43c51ecbec64505e33157feeb499dc305a53b0
tia-lab <tia@eki.studio>
2026-01-16 09:02:29 +0100

    chore: clean up cached file struct

--- deprecated/orsx/src/traits/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod batch_table_query;
-pub mod orsx_migrate;
-pub mod table_query;
-
-pub use batch_table_query::BatchTableQuery;
-pub use orsx_migrate::OrsxMigrate;
-pub use table_query::TableQuery;
--- deprecated/orsx/src/types/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod compressed;
-pub mod field_type;
-
-pub use compressed::Compressed;
-pub use field_type::FieldType;
